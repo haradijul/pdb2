ac8642b7a15398a26ff8e515a32c5ceaf9317f9d
haradi <zulfahri123@gmail.com>
2022-12-04 19:54:38 +0700

    update

--- WordCount.java
+++ WordCount.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
 import java.math.BigInteger;
+import java.util.Collections;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
